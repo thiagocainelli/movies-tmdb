cb75e9825965711974a6d312d8c30652467d80c3
Thiago Cainelli <thiagocainelli@gmail.com>
2023-12-21 19:41:03 -0300

    Add search input

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "react-query";
 import { fetchMovies } from "@/fetchMovies"
 import { IconStarFilled } from "@tabler/icons-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 type MoviesProps = {
     id: number,
@@ -13,38 +14,58 @@ type MoviesProps = {
     vote_average: number,
 }
 
-function MoviesList () {
+function MoviesList() {
 
     const { data, isLoading, isError } = useQuery("movies", fetchMovies)
+    const [search, setSearch] = useState<string>("");
 
-    console.log(data);
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+    
+    const filteredMovies = data?.results.filter((movie: MoviesProps) => {
+        return movie.title.toLowerCase().includes(search.toLowerCase())
+    })
 
-    if(isLoading) {
+    if (isLoading) {
         return <p className="text-center text-2xl mt-10">Carregando lista de filmes...</p>
     }
 
-    if(isError) {
-        return <p className="text-center text-2xl mt-10">Falha ao carregar a lista de filmes</p>	
+    if (isError) {
+        return <p className="text-center text-2xl mt-10">Falha ao carregar a lista de filmes</p>
     }
 
     return (
         <section className="flex items-center justify-center flex-wrap gap-8 py-10">
-            {data.results.map((movie: MoviesProps) => (
-                <div className="bg-zinc-800 rounded-md p-3 flex flex-col items-center justify-center border border-transparent transition-all hover:border-zinc-50 max-w-[95%]" key={movie.id}>
-                    <h2 className="mb-4 text-center text-xl font-semibold">{movie.title}</h2>
+            <div className="w-full max-w-[1200px] flex items-center justify-center">
+                <input 
+                    type="text" 
+                    placeholder="Pesquisar Filme..."
+                    className="w-[50%] p-3 rounded-md bg-zinc-800 text-white focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-offset-2 focus:ring-offset-zinc-800"
+                    value = {search}
+                    onChange={handleSearch}
+                />
+            </div>
 
-                    <Image className="rounded-md" src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} width={300} height={300} />
-                    
-                    <div className="flex items-center justify-between w-full mt-2">
-                        <span>Avaliação:</span> 
-                        <p className="flex items-center gap-2"> <IconStarFilled className="w-5 h-5 text-yellow-300" /> {movie.vote_average.toFixed(1)}</p>
+            <div className="flex items-center justify-center flex-wrap gap-8 max-w-[1200px]">
+                {filteredMovies.map((movie: MoviesProps) => (
+                    <div className="bg-zinc-800 rounded-md p-3 flex flex-col items-center justify-center border border-transparent transition-all hover:border-zinc-50 w-[330px]" key={movie.id}>
+                        <h2 className="mb-4 text-center text-xl font-semibold">{movie.title}</h2>
+
+                        <Image className="rounded-md" src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} width={300} height={300} />
+
+                        <div className="flex items-center justify-between w-full mt-2">
+                            <span>Avaliação:</span>
+                            <p className="flex items-center gap-2"> <IconStarFilled className="w-5 h-5 text-yellow-300" /> {movie.vote_average.toFixed(1)}</p>
+                        </div>
+
+                        <Link href={`/${movie.id}`}>
+                            <button className="bg-blue-500 rounded-md p-2 mt-5 hover:bg-blue-400 hover:scale-105">Mais detalhes</button>
+                        </Link>
                     </div>
+                ))}
+            </div>
 
-                    <Link href={`/${movie.id}`}>
-                        <button className="bg-blue-500 rounded-md p-2 mt-5 hover:bg-blue-400 hover:scale-105">Mais detalhes</button>
-                    </Link>
-                </div>
-            ))}
         </section>
     )
 }
--- src/fetchMovies.ts
+++ src/fetchMovies.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export const fetchMovies = async () => {
-     
-    const response = await axios.get("https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1&api_key=b91aaf827797d117cc8088b6dbf681c8");
-
-    return response.data;
+    try {
+        const response = await axios.get("https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1&api_key=b91aaf827797d117cc8088b6dbf681c8");
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
